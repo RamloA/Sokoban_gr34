f39ccc90a5982421824fdc789946896f9686feef
RamloA <36705833+RamloA@users.noreply.github.com>
2018-10-28 21:56:35 +0100

    3

--- my_code.py
+++ my_code.py
@@ -12,17 +12,22 @@ mB = ev3.LargeMotor('outB')
 lightSensorLeft = ev3.ColorSensor('in1')
 lightSensorRight = ev3.ColorSensor('in2')
 
+lightSensorFront = ev3.LightSensor('in3')
+US = ev3.UltrasonicSensor('in4')
+
 BASE_SPEED = 30
 TURN_SPEED = 80
-
 
 mA.run_direct()
 mB.run_direct()
 
+lightSensorRight.color()
+lightSensorLeft.color()
+
 def forward():
     while True:
-        sensorLeft = lightSensorLeft.color
-        sensorRight = lightSensorRight.color
+        sensorLeft = lightSensorLeft.MODE_COL_COLOR
+        sensorRight = lightSensorRight.MODE_COL_COLOR
 
         if sensorLeft == 6 and sensorRight == 6:
             mA.duty_cycle_sp = BASE_SPEED
@@ -31,7 +36,6 @@ def forward():
             mA.duty_cycle_sp = 0
             mB.duty_cycle_sp = 0
             break
-
 
 
 def reverse():
@@ -43,36 +47,26 @@ def reverse():
             mB.polarity = "inversed"
             mA.duty_cycle_sp = BASE_SPEED
             mB.duty_cycle_sp = BASE_SPEED
-            elif sensorLeft==6 and sensorRight ==6:\
-                mA.duty_cycle_sp = BASE_SPEED
+        elif sensorLeft == 6 and sensorRight == 6:
+            mA.polarity = "normal"
+            mB.polarity = "normal"
+            mA.duty_cycle_sp = BASE_SPEED
             mB.duty_cycle_sp = BASE_SPEED
             if sensorLeft == 1 and sensorRight == 1:
                 mA.duty_cycle_sp = 0
                 mB.duty_cycle_sp = 0
+                break
 
 
-def sensor
+def sensor_read():
+    while True:
+        us_readings = US.distance_centimeters_continuous
+        sensorFront = lightSensorFront.Reflect
+
+        print("Ultrasound    " + US_readings)
+        print("SensorFront   " + sensorFront)
 
 
-while True:
-    sensorLeft = lightSensorLeft.color
-    sensorRight = lightSensorRight.color
-    print("sensorLeft: ", sensorLeft, " sensorRight: ", sensorRight)
-    if sensorLeft == 6 and sensorRight == 6:
-        mA.polarity = "normal"
-        mB.polarity = "normal"
-        mA.duty_cycle_sp = BASE_SPEED
-        mB.duty_cycle_sp = BASE_SPEED
-    # elif sensorLeft == 1 and sensorRight == 1:
-    # mA.duty_cycle_sp = 0
-    # mB.duty_cycle_sp = 0
-    # mA.polarity = "inversed"
-    # mB.polarity = "inversed"
-    # mA.duty_cycle_sp = BASE_SPEED
-    # mB.duty_cycle_sp = BASE_SPEED
-    # if sensorLeft == 1 and sensorRight == 1:
-        #mA.duty_cycle_sp = 0
-        #mB.duty_cycle_sp = 0
 
 
 
