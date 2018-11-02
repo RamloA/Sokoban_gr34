dc0dae74dcf12378c3340bc84213682998eb1635
RamloA <36705833+RamloA@users.noreply.github.com>
2018-11-02 12:24:39 +0100

    3

--- my_code.py
+++ my_code.py
@@ -6,18 +6,25 @@ import signal
 import numpy as np
 
 
-# Connect two motors and two (different) light sensors
+# Connect two motors and color and light sensors
 mA = ev3.LargeMotor('outA')
 mB = ev3.LargeMotor('outB')
-
-lightSensorLeft = ev3.ColorSensor('in1')
-lightSensorRight = ev3.ColorSensor('in2')
-
+lightSensorRight = ev3.ColorSensor('in1')
+lightSensorLeft = ev3.ColorSensor('in2')
 lightSensorFront = ev3.LightSensor('in3')
-US = ev3.UltrasonicSensor('in4')
 
-BASE_SPEED = 30
+
+print(mA.connected)
+print(mB.connected)
+print(lightSensorLeft.connected)
+print(lightSensorRight.connected)
+print(lightSensorFront.connected)
+
+BASE_SPEED = 60
 TURN_SPEED = 80
+BASE_TURN_SPEED = 30
+CalI_TURN_SPEED = 50
+CALI_SPEED = 15
 
 mA.run_direct()
 mB.run_direct()
@@ -66,12 +73,13 @@ def reverse():
 
 def sensor_read():
     while True:
-        #us_readings = US.distance_centimeters_continuous
-        sensorFront = lightSensorFront.value()
-
-        #print("Ultrasound    " + US_readings)
-        print("SensorFront   " + sensorFront)
-
+        sensorFront = lightSensorFront.reflected_light_intensity
+        sensorLeft = lightSensorLeft.color
+        sensorRight = lightSensorRight.color
+        print("sensorFront")
+        print(sensorFront)
+        print("sensorRight")
+        print(sensorRight)
 
 
 
@@ -81,13 +89,58 @@ def forwardSF():
         sensorLeft = lightSensorLeft.color
         sensorRight = lightSensorRight.color
 
+
+        white = sensorFront
+
         if sensorLeft == 6 and sensorRight == 6:
             mA.duty_cycle_sp = BASE_SPEED
             mB.duty_cycle_sp = BASE_SPEED
-        elif sensorFront > 300  and sensorFront < 400:
+            black = sensorFront / 2
+        elif black < sensorFront:
             mA.duty_cycle_sp = 0
             mB.duty_cycle_sp = 0
             break
 
 
+def forward_Calibration():
+    while True:
+        sensorFront = lightSensorFront.reflected_light_intensity
+        sensorLeft = lightSensorLeft.color
+        sensorRight = lightSensorRight.color
+
+        if sensorLeft == 6 and sensorRight == 6:
+            mB.duty_cycle_sp = BASE_SPEED
+            mA.duty_cycle_sp = BASE_SPEED
+
+        if sensorLeft == 1 and sensorRight == 6:
+            mA.duty_cycle_sp = CalI_TURN_SPEED
+            mB.duty_cycle_sp = CALI_SPEED
+
+        if sensorLeft == 6 and sensorRight == 1:
+            mB.duty_cycle_sp = CalI_TURN_SPEED
+            mA.duty_cycle_sp = CALI_SPEED
+        if sensorFront < 50:
+            mB.duty_cycle_sp = 0
+            mA.duty_cycle_sp = 0
+            break
+
+
+def right_Turn():
+    forward_Calibration()
+    while True:
+        sensorFront = lightSensorFront.reflected_light_intensity
+        sensorLeft = lightSensorLeft.color
+        sensorRight = lightSensorRight.color
+
+        if sensorFront < 50:
+            mB.duty_cycle_sp = TURN_SPEED
+            mB.duty_cycle_sp = BASE_TURN_SPEED
+
+        if sensorLeft ==
+
+        if sensorLeft == sensorRight == 1:
+            mB.duty_cycle_sp = BASE_SPEED
+            mA.duty_cycle_sp = BASE_SPEED
+        if sensorLeft == 6 and sensorRight == 6:
+            forward_Calibration()
 
