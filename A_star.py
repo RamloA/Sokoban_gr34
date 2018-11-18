7b0ba32b7fcb86472ca2b7062a6f8508beeeb252
RamloA <36705833+RamloA@users.noreply.github.com>
2018-11-18 22:41:52 +0100

    3

--- A_star.py
+++ A_star.py
@@ -1,11 +1,26 @@
 import numpy as np
+import heapq
+
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
+
+    def empty(self):
+        return len(self.elements) == 0
+
+    def put(self, item, priority):
+        heapq.heappush(self.elements, (priority, item))
+
+    def get(self):
+        return heapq.heappop(self.elements)[1]
+
 class Nodes:
     """"
     def _init__(self):
         self.nodes = []
     """
     def __init__(self):
-            self.nodes = np.empty([8,7])
+            self.nodes = []
 
     def in_bounds(self, id):
             #self.rows = self.nodes.shape[0]
@@ -14,12 +29,8 @@ class Nodes:
             return 0 <= x < self.width and 0 <= y < self.height
 
     def neighbours(self, index):
-        """
-        Input: 
-        M: should be the the array of the map 
-        """""
-        (x, y) = index
-        results = []
+
+        [x, y] = index
         neighbours_res = [(x-1, y), (x, y-1), (x+1, y), (x, y+1)]
         """
         for neigbour in neighbours_res:
@@ -30,11 +41,45 @@ class Nodes:
         return neighbours_res
 
 
-    def heuristics(self, a, b):
-        (x1, y1) = a
-        ()
 
 
+def heuristics(a, b):
+    [x1, y1] = a
+    [x2, y2] = b
+    dist_h = (x1-x2) + (y1-y2)
+    return abs(dist_h)
 
+def a_star(plan, start, goal):
+    """
+          Input:
+          Plan: should be the the array of the map
+          open_list: is the listed of nodes we should visit
+          visited: nodes we have visited
+          We know distance between neighbours will always be 1 -> therefor we can hardcode the distance
+    """
+    open_List = PriorityQueue()
+    open_List.put(start, 0)
+    Visited = {}
+    cost = {}
+    Visited[start] = 0
+    cost[start] = 0
+    cost_neighbours = 1
+
+    while not open_List.empty():
+        current = open_List.get()
+
+        if current == goal:
+            break
+
+        for next_node in plan.neighbours(current):
+            if plan.nodes[next_node] == 1 or plan.nodes[next_node] == 2:
+                new_cost = cost[current] + cost_neighbours
+
+                if next_node not in cost or new_cost < cost[next_node]:
+                    cost[next_node] = new_cost
+                    priority = new_cost + heuristics(current, goal)
+                    open_List.put(next_node, priority)
+                    Visited[next_node] = current
+    return Visited, cost
 
 
--- Search_path.py
+++ Search_path.py
@@ -1,4 +1,4 @@
-from A_star import Nodes
+from A_star import *
 import numpy as np
 
 A_map = Nodes()
@@ -6,19 +6,33 @@ A_map = Nodes()
 #A_map = [1, 1, 1, 1]
 
 
-A_map.nodes = (
-    [[0, 0, 0, 0, 0, 0, 0],
-     [0, 1, 1, 1, 2, 1, 0],
-     [0, 1, 1, 1, 1, 1, 0],
-     [0, 2, 1, 2, 1, 2, 0],
-     [0, 1, 1, 0, 1, 0, 0],
-     [0, 1, 3, 2, 1, 1, 0],
-     [0, 0, 0, 1, 3, 1, 0],
-     [0, 0, 0, 1, 0, 1, 0]])
+A_map.nodes = np.array([[0, 0, 0, 0, 0, 0, 0],
+                        [0, 1, 1, 1, 2, 1, 0],
+                        [0, 1, 1, 1, 1, 1, 0],
+                        [0, 2, 1, 2, 1, 2, 0],
+                        [0, 1, 1, 0, 1, 0, 0],
+                        [0, 1, 3, 2, 1, 1, 0],
+                        [0, 0, 0, 1, 3, 1, 0],
+                        [0, 0, 0, 1, 0, 1, 0],
+                        [0, 1, 3, 3, 1, 1, 0],
+                        [0, 1, 3, 1, 1, 1, 0],
+                        [0, 1, 1, 1, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0]])
 
+print(A_map.nodes.shape)
 
-index = (1, 1)
-N_R = A_map.neighbours(index)
-print(N_R)
+#index = [1, 1]
+"""
+#ALLE INDEX skal skrives med ()
+"""
+index = (1,1)
+print(A_map.neighbours(index))
 
+print("HER:   ", A_map.nodes[1, 1])
 
+Start, Goal = (8, 3), (1, 4)
+
+Vis, cost = a_star(A_map, Start, Goal)
+
+print("Visited: ", Vis)
+print(cost)
